e778d8cb69c7f1befc9c719afa0eeb34dd29263d
Finlay McCourt <finlaymccourt@gmail.com>
2019-04-03 16:54:31 +0100

    multiple E2E tests.

--- tests/test_appetiser.py
+++ tests/test_appetiser.py
@@ -1,3 +1,7 @@
+from PIL import (
+        Image,
+        )
+
 
 def test_ping(appetiser_client):
     response = appetiser_client.get('/ping')
@@ -39,9 +43,14 @@ def test_convert_invalid_operation(appetiser_client):
     response = appetiser_client.post('/convert', json=post_json)
     assert response.json == expected_response
 
-def test_convert_jpg(appetiser_client, fixtures_dir, temp_dir):
-    img_name = 'fol_73r_detail'
-    img_src = fixtures_dir.joinpath('Ms_W_102', img_name).with_suffix('.jpg')
+def _convert_test_runner(
+        appetiser_client, 
+        temp_dir,
+        img_src,
+        optimisation,
+        thumb_sizes
+        ):
+    img_name = img_src.stem
     img_dest = temp_dir.joinpath(img_name).with_suffix('.jp2')
     thumb_dir = temp_dir
     post_json = {
@@ -49,21 +58,78 @@ def test_convert_jpg(appetiser_client, fixtures_dir, temp_dir):
         'jobId': img_name + '_job',
         'source': str(img_src),
         'destination': str(img_dest),
-        'thumbSizes': [1000, 2000, 30, 600],
+        'thumbSizes': thumb_sizes,
         'thumbDir': str(thumb_dir),
         'operation': 'ingest',
-        'optimisation': 'kdu_med',
-        'origin': 'an_origin',
+        'optimisation': optimisation,
+        'origin': img_name + '_origin',
     }
-
-    expected_response = {
-        'imageId': 'fol.73r_detail',
-        'jobId': 'fol.73r_detail_job',
-        'optimisation': 'an_optimisation',
-        'origin': 'an_origin',
-    }
-
+    orig_image = Image.open(img_src)
     response = appetiser_client.post('/convert', json=post_json)
-    print(response.json)
-    print(expected_response)
-    assert response.json == expected_response
+    assert response.json['imageId'] == img_name
+    assert response.json['jobId'] == img_name + '_job'
+    assert response.json['origin'] == img_name + '_origin'
+    assert response.json['jp2'] == str(img_dest)
+    assert response.json['height'] == orig_image.height
+    assert response.json['width'] == orig_image.width
+    if thumb_sizes: 
+        for size, thumb_dict in zip(sorted(thumb_sizes), sorted(response.json['thumbs'], key=lambda x: max(x['height'], x['width']))):
+            assert size == max(thumb_dict['height'], thumb_dict['width'])
+            assert str(thumb_dir.joinpath('{}_{}'.format(img_name, size)).with_suffix('.jpg')) == thumb_dict['path']
+    else: 
+        assert response.json['thumbs'] == []
+
+
+def test_convert_jpg(appetiser_client, fixtures_dir, temp_dir):
+    _convert_test_runner(
+            appetiser_client, 
+            temp_dir, 
+            fixtures_dir.joinpath('Ms_W_102/fol_73r_detail.jpg'), 
+            'kdu_med', 
+            [30, 600, 400, 100]
+            )
+
+def test_convert_tiff(appetiser_client, fixtures_dir, temp_dir):
+    _convert_test_runner(
+            appetiser_client, 
+            temp_dir, 
+            fixtures_dir.joinpath('Ms_W_102/fol_73v_detail.tiff'), 
+            'kdu_med', 
+            [30, 600, 400, 100]
+            )
+
+def test_convert_bmp(appetiser_client, fixtures_dir, temp_dir):
+    _convert_test_runner(
+            appetiser_client, 
+            temp_dir, 
+            fixtures_dir.joinpath('Ms_W_102/fol_74r_detail.bmp'), 
+            'kdu_med', 
+            [30, 600, 400, 100]
+            )
+
+def test_convert_pbm(appetiser_client, fixtures_dir, temp_dir):
+    _convert_test_runner(
+            appetiser_client, 
+            temp_dir, 
+            fixtures_dir.joinpath('Ms_W_102/fol_74v_detail.pbm'), 
+            'kdu_med', 
+            [30, 600, 400, 100]
+            )
+
+def test_convert_pgm(appetiser_client, fixtures_dir, temp_dir):
+    _convert_test_runner(
+            appetiser_client, 
+            temp_dir, 
+            fixtures_dir.joinpath('Ms_W_102/fol_75r_detail.pgm'), 
+            'kdu_med', 
+            [30, 600, 400, 100]
+            )
+
+def test_convert_ppm(appetiser_client, fixtures_dir, temp_dir):
+    _convert_test_runner(
+            appetiser_client, 
+            temp_dir, 
+            fixtures_dir.joinpath('Ms_W_102/fol_75v_detail.ppm'), 
+            'kdu_med', 
+            [30, 600, 400, 100]
+            )
